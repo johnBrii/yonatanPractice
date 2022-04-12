ec066e22fdcd5699552b79c62033df2d650fb568
Yonatan <yoniw7@gmail.com>
2022-04-12 10:26:01 +0300

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import exampleModule from "./modules/exampleModule"
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    modules: exampleModule
+})
+
+
+export default store;
