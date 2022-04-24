702e3a17cd242d1beddb6603b4b1af35c6f78be0
Yonatan <yoniw7@gmail.com>
2022-04-24 11:29:44 +0300

    added dialog

---
+++ src/components/generics/PopUp.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog max-width="600px" :value="dialog" @click:outside="close">
+    <v-card>
+      <v-card-title>{{ text }}</v-card-title>
+
+      <v-slot name="content"></v-slot>
+      <v-text-field @keyup="userInput"></v-text-field>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "PopUp",
+  props: { dialog: Boolean, text: String },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container align-center fill-height mt-n5>
+    <v-spacer></v-spacer>
     <v-data-table
       light
       :headers="headers"
@@ -16,21 +17,35 @@
           v-model="search"
           label="search"
           class="mx-4"
-        ></v-text-field> </template></v-data-table
-  ></v-container>
+        ></v-text-field>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <div class="d-flex">
+          <v-icon @click="deleteItem(item)">mdi-delete</v-icon>
+          <v-icon @click="editItem(item)">mdi-pencil</v-icon>
+        </div>
+      </template></v-data-table
+    >
+    <v-spacer></v-spacer>
+    <popUp v-if="edit" :dialog="edit" :text="'edit'" @close="edit = false" />
+  </v-container>
 </template>
 
 <script>
+import PopUp from "../components/generics/PopUp.vue";
 export default {
+  components: { PopUp },
   data() {
     return {
       search: "",
+      edit: false,
       posts: [],
       headers: [
         { text: "body", value: "body" },
         { text: "id", value: "id" },
         { text: "title", value: "title" },
         { text: "userID ", value: "userId" },
+        { text: "actions ", value: "actions" },
       ],
     };
   },
@@ -39,6 +54,9 @@ export default {
       await this.$store.dispatch("getAll");
       this.posts = this.$store.state.postModule.posts; // is getters better here?
       console.log(this.posts);
+    },
+    editItem() {
+      this.edit = true;
     },
   },
 
