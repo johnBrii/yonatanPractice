e6c7d107b8b64244e3d8c19b575670212d0e675b
Yonatan <yoniw7@gmail.com>
2022-04-25 14:40:40 +0300

    pr

--- src/views/vuetifyTrain.vue
+++ src/views/vuetifyTrain.vue
@@ -1,5 +1,39 @@
 <template>
-  <v-layout color="primary"> </v-layout>
+  <v-container fluid fill-height grow class="pa-0">
+    <v-row no-gutters>
+      <v-col class="d-none d-lg-flex d-xl-flex green"
+        ><v-card height="26vh" color="red"></v-card>
+      </v-col>
+      <v-col class="d-none d-md-flex d-lg-flex d-xl-flex red"
+        ><v-card height="26vh" color="green"></v-card>
+      </v-col>
+      <v-col class="d-flex blue">
+        <v-card height="26vh" color="red"></v-card
+      ></v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="d-none d-lg-flex d-xl-flex green"
+        ><v-card height="26vh" color="red"></v-card>
+      </v-col>
+      <v-col class="d-none d-md-flex d-lg-flex d-xl-flex red"
+        ><v-card height="26vh" color="green"></v-card>
+      </v-col>
+      <v-col class="d-flex blue">
+        <v-card height="26vh" color="red"></v-card
+      ></v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="d-none d-lg-flex d-xl-flex green"
+        ><v-card height="26vh" color="red"></v-card>
+      </v-col>
+      <v-col class="d-none d-md-flex d-lg-flex d-xl-flex red"
+        ><v-card height="26vh" color="green"></v-card>
+      </v-col>
+      <v-col class="d-flex blue">
+        <v-card height="26vh" color="red"></v-card
+      ></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
