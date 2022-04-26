aa9ed1f5c8442f3a7d3b5efed991875e5e62663d
Yonatan <yoniw7@gmail.com>
2022-04-26 10:49:56 +0300

    cleaner code for breaking points

--- src/views/vuetifyTrain.vue
+++ src/views/vuetifyTrain.vue
@@ -1,32 +1,32 @@
 <template>
   <v-container fluid fill-height class="pa-0">
     <v-row wrap no-gutters>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
-        ><v-card height="26vh" color="red"></v-card>
-      </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
+      <v-col cols="12" sm="6" lg="4"
         ><v-card height="26vh" color="green"></v-card>
       </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
-        ><v-card height="26vh" color="blue"></v-card>
-      </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
+      <v-col cols="12" sm="6" lg="4"
         ><v-card height="26vh" color="red"></v-card>
       </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
-        ><v-card height="26vh" color="green"></v-card>
-      </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
+      <v-col cols="12" sm="6" lg="4"
         ><v-card height="26vh" color="blue"></v-card>
       </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
+      <v-col cols="12" sm="6" lg="4"
+        ><v-card height="26vh" color="green"></v-card>
+      </v-col>
+      <v-col cols="12" sm="6" lg="4"
         ><v-card height="26vh" color="red"></v-card>
       </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
+      <v-col cols="12" sm="6" lg="4"
+        ><v-card height="26vh" color="blue"></v-card>
+      </v-col>
+      <v-col cols="12" sm="6" lg="4"
         ><v-card height="26vh" color="green"></v-card>
       </v-col>
-      <v-col cols="12" md="6" sm="6" xl="4" lg="4"
-        ><v-card height="26vh" color="blue"></v-card>
+      <v-col cols="12" sm="6" lg="4"
+        ><v-card height="26vh" color="red"></v-card>
+      </v-col>
+      <v-col cols="12" sm="6" lg="4">
+        <v-card height="26vh" color="blue"></v-card>
       </v-col>
     </v-row>
   </v-container>
